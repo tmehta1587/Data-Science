ca082cb2220b84c3cb89337dde58c0e2a89ec33b
tmehta1587 <tmehta1587@gmail.com>
2025-12-24 18:26:23 +0700

    c

--- Module 1/Visualization with data science IV/acp.py
+++ Module 1/Visualization with data science IV/acp.py
@@ -5,6 +5,8 @@ import seaborn as sns
 import matplotlib as plt
 
 Penguins_df = pd.read_csv('Penguins_Data.csv')
+print(Penguins_df.columns)
+Penguins_df.columns = Penguins_df.columns.str.strip()
 
 sns.histplot (Penguins_df['Culmen length(mm)' ], bins=10, kde=True) 
 
