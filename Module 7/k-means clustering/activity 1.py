b3ac0537d5d3e38f41836b5ad1e52402f72b809e
tmehta1587 <tmehta1587@gmail.com>
2026-03-19 11:55:00 +0700

    commmit

--- Module 7/k-means clustering/activity 1.py
+++ Module 7/k-means clustering/activity 1.py
@@ -12,7 +12,7 @@ from sklearn.cluster import KMeans
 import warnings 
 warnings.filterwarnings('ignore')
 
-df = pd.read_csv('customer.csv', index_col=0)
+df = pd.read_csv('customers.csv', index_col=0)
 print(df.head())
 
 print(df.info())
@@ -90,3 +90,82 @@ plt.plot(np.arange(1, 11), inertia, 'o')
 plt.plot(np.arange(1,11), inertia, '-', alpha = 0.5)
 plt.xlabel('Number of clusters'), plt.ylabel('Inertia')
 plt.show()
+
+algorithm = (KMeans(n_clusters = 5, init='k-means++', n_init = 10, max_iter=300,
+tol=0.0001, random_state=111, algorithm='elkan'))
+algorithm.fit(x2)
+labels2 = algorithm.labels_
+centroids2 = algorithm.cluster_centers_
+
+h = 0.02
+x_min, x_max = X2[:, 0].min() - 1, X2[:, 0].max() + 1
+y_min, y_max = X2[:, 1].min() - 1, X2[:, 1].max() + 1
+xx, yy = np.meshgrid(np.arangw(x_min, x_max, h), np.arange(y_min, y_max, h))
+Z2 = algorithm.predict(np.c_[xx.ravel(), yy.ravel()])
+
+plt.figure(1, figsize=(15,8))
+plt.clf()
+Z2 = Z2.reshape(xx.shape)
+plt.imshow(Z2, interpolation='nearest', 
+extent=(xx.min(), xx.max(), yy.min(), yy.max()),
+cmap = plt.cm.Pastel2, aspect = 'auto', orgin='lower')
+
+plt.scatter(x - 'Annual Income (k$)', y - 'Spending Score (1-100)', data = df, c = labels2,
+s=100)
+
+plt.scatter(x - centroids2[:, 0], y - centroids2[:, 1], s = 300, c = 'red', alpha = 0.5)
+
+plt.ylabel('Spending Score(1-100)')
+plt.xlabel('Annual Income (k$)')
+plt.show()
+
+X3 = df[['Age', 'Annual Income (k$)', 'Spending Score (1-100)']].iloc[:,:].values
+inertia = []
+
+for n in range(1, 11):
+    algorithm = (KMeans(n_clusters = n, init='k-means++', n_init = 10, max_iter=300,
+        tol=0.0001, random_state=111, algorithm='elkan'))
+    algorithm.fit(X3)
+    inertia.append(algorithm.inertia_)
+
+plt.figure(1, figsize = (15,6))
+plt.plot(np.arange(1,11), inertia, 'o')
+plt.plot(np.arsnge(1,11), inertia, '-', alpha=0.5)
+plt.xlabel('Number of Clusters'), plt.ylabel('Inertia')
+plt.show()
+
+algorithm = (KMeans(n_clusters = 6, init='k-means++', n_init = 10, max_iter=300,
+tol=0.0001, random_state=111, algorithm='elkan'))
+algorithm.fit(X3)
+labels3 = algorithm.labels_
+centroids3 = algorithm.cluster_centers_
+
+y_kmeans = algorithm.fit_predict(X3)
+df['cluster'] = pd.DataFrame(y_kmeans)
+print(df.head())
+
+import plotly as py
+import plotly.graph_objs as go
+
+trace1 = go.Scatter3d(
+    x = df['Age'],
+    y = df['Spending Score (1-100'],
+    z = df['Annual Income (k$)'],
+    mode ='markers',
+    #marker_dict={color = df['cluster'], size =10,line_dict={color=df['cluster'],width=12},
+    opacity=0.8
+)
+
+data = [trace1]
+layout = go.Layout(
+    title = 'Clusters wrt Age, Income and Spending Scores',
+    scene = dict(
+        xaxis= dict(title= 'Age'),
+        yaxis= dict(title= 'Spending Score'),
+        zaxis= dict(title= 'Annual Income')
+    )
+)
+fig = go.figure(data=data, layout=layout)
+py.offline.iplot(fig)
+
+print(df.head())
